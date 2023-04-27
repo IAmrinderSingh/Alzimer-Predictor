a450fdaa1cec7f34877335c401009db521a77de5
IAmrinderSingh <amrindersingh1703@gmail.com>
2023-04-27 12:59:43 +0530

    Predicting Disease

--- server.py
+++ server.py
@@ -12,16 +12,43 @@ def home():
 def predict():
 
     if request.method == 'POST':
+
         image_data = request.files["imgInp"]
+
         if(not image_data):
+
             return render_template('index.html',output="Please Choose Image")
         
-        directory = os.path.join('static/uploads/',image_data.filename);
+        directory = os.path.join('./Upload/',image_data.filename);
 
-        print(directory);
+        #model
+        import cv2
+        import tensorflow as tf
+        from tensorflow.keras.preprocessing import image
+        from tensorflow.keras import models
+        import numpy as np
+        model = tf.keras.models.load_model('alzheimers.h5',compile=False)
 
-        # output=
-        return render_template('index.html',output=output)
+        img=cv2.imread(directory)
+        img=cv2.resize(img,(150,150))
+        img_array=np.array(img)
+        # img_array.shape
+
+        img_array=img_array.reshape(1,150,150,3)
+        # img_array.shape
+
+        a=model.predict(img_array)
+        indices=a.argmax()
+        if indices==0:
+            o="Mild Demented"
+        elif indices==1:
+            o="Moderate Demented"
+        elif indices==2:
+            o="Non Demented"
+        else:
+            o="Very Mild Demented"
+
+        return render_template('index.html',output=o)
     
     return render_template('index.html')
 
@@ -32,8 +59,6 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
-
-
 
 if __name__ == '__main__':
     app.run()
